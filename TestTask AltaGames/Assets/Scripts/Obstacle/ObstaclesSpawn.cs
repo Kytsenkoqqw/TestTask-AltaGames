b74834ae5ef5f8b16ca4ecf352c63d8fd5ad9da0
Kytsenkoqqw <kitsmaxqqwwwe855@gmail.com>
2024-01-16 16:36:25 +0200

    Update SpawnSystem

--- TestTask AltaGames/Assets/Scripts/Obstacle/ObstaclesSpawn.cs
+++ TestTask AltaGames/Assets/Scripts/Obstacle/ObstaclesSpawn.cs
@@ -1,14 +1,18 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class ObstaclesSpawn : MonoBehaviour
 {
     [SerializeField] private Transform _spawnPoint;
-    [SerializeField] private GameObject _obstaclePrefab;
+    [SerializeField] private Transform _obstaclePrefab;
     [SerializeField] private float _spawnRadius;
     [SerializeField] private int _amountObstacles;
+    [SerializeField] private float _radiusObstacle;
+    [SerializeField] private LayerMask _layerMaskObstacle;
+    private List<Transform> _transformsPoint = new List<Transform>();
 
     private void Start()
     {
@@ -17,13 +21,21 @@ public class ObstaclesSpawn : MonoBehaviour
 
     private void SpawnObstacles()
     {
-        for (int i = 0; i < _amountObstacles; i++)
+       // int j = 0;
+        for (int i = 0, j = 0; i < _amountObstacles && j < 1000; i++, j++)
         {
-            Vector3 randomCirclePoint = UnityEngine.Random.insideUnitCircle.normalized * _spawnRadius;
+            Vector3 randomCirclePoint = UnityEngine.Random.insideUnitCircle * _spawnRadius;
             Vector3 spawnPoint = new Vector3(randomCirclePoint.x + _spawnPoint.position.x, _spawnPoint.position.y, randomCirclePoint.y + _spawnPoint.position.z);
-            Instantiate(_obstaclePrefab, spawnPoint, Quaternion.identity);
 
-          
+            var spawnedObstacle =  Instantiate(_obstaclePrefab, spawnPoint, Quaternion.identity);
+            _transformsPoint.Add(spawnedObstacle);
+            Collider[] colliders = Physics.OverlapSphere(spawnedObstacle.position, _radiusObstacle, _layerMaskObstacle);
+
+            if (colliders.Length > 1)
+            {
+                Destroy(spawnedObstacle.gameObject);
+                _amountObstacles++;
+            }
         }
     }
 }
